ee654fd9ab2c4b7d2b95c7997852c37e64639d49
ChrisNade <christopheromonade@gmail.com>
2022-10-18 21:53:47 -0700

    betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+#include <main.h>
 
 /**                                                                                                                                   
- * main -  prints Holberton, followed by a new line                                                                                   
- *
- *  * Return: Always 0 (Success)                                                                                                         
- *
- *   */ 
+* main -  prints Holberton, followed by a new line                                                                                   
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-
 	printf("_putchar\n");
+
 	return (0);
 }
